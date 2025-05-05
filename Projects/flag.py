9452d4fc786f6123383a3b66399369df3dbbe5b8
Cclark1234 <christianclark@seattleacademy.org>
2025-05-05 16:29:11 +0000

    turtle racing

--- Projects/flag.py
+++ Projects/flag.py
@@ -89,9 +89,136 @@ t.forward(h)
 t.left(90)
 t.end_fill()
 
+# move to stripe 6
+t.goto(-250, -150)
+h = 50
+# stripe 1
+t.color("white")
+t.begin_fill()
+t.forward(500)
+t.left(90)
+t.forward(h)
+t.left(90)
+t.forward(500)
+t.left(90)
+t.forward(h)
+t.left(90)
+t.end_fill()
+
+# move to stripe 7
+t.goto(-250, -200)
+h = 50
+# stripe 1
+t.color("white")
+t.begin_fill()
+t.forward(500)
+t.left(90)
+t.forward(h)
+t.left(90)
+t.forward(500)
+t.left(90)
+t.forward(h)
+t.left(90)
+t.end_fill()
+
+# move to stripe 8
+t.goto(-250, -250)
+h = 50
+# stripe 1
+t.color("white")
+t.begin_fill()
+t.forward(500)
+t.left(90)
+t.forward(h)
+t.left(90)
+t.forward(500)
+t.left(90)
+t.forward(h)
+t.left(90)
+t.end_fill()
+
+# move to stripe 9
+t.goto(-250, -300)
+h = 50
+# stripe 1
+t.color("red")
+t.begin_fill()
+t.forward(500)
+t.left(90)
+t.forward(h)
+t.left(90)
+t.forward(500)
+t.left(90)
+t.forward(h)
+t.left(90)
+t.end_fill()
+
+# move to stripe 10
+t.goto(-250, -350)
+h = 50
+# stripe 1
+t.color("white")
+t.begin_fill()
+t.forward(500)
+t.left(90)
+t.forward(h)
+t.left(90)
+t.forward(500)
+t.left(90)
+t.forward(h)
+t.left(90)
+t.end_fill()
+
+# move to stripe 11
+t.goto(-250, 150)
+h = 50
+# stripe 1
+t.color("red")
+t.begin_fill()
+t.forward(500)
+t.left(90)
+t.forward(h)
+t.left(90)
+t.forward(500)
+t.left(90)
+t.forward(h)
+t.left(90)
+t.end_fill()
+
+# move to stripe 12
+t.goto(-250, 200)
+h = 50
+# stripe 1
+t.color("white")
+t.begin_fill()
+t.forward(500)
+t.left(90)
+t.forward(h)
+t.left(90)
+t.forward(500)
+t.left(90)
+t.forward(h)
+t.left(90)
+t.end_fill()
+
+# move to stripe 13
+t.goto(-250, 250)
+h = 50
+# stripe 1
+t.color("red")
+t.begin_fill()
+t.forward(500)
+t.left(90)
+t.forward(h)
+t.left(90)
+t.forward(500)
+t.left(90)
+t.forward(h)
+t.left(90)
+t.end_fill()
 
 # blue square
-t.goto(-250, 100)
+t.goto(-250, 250)
 t.color("blue")
 t.begin_fill()
 t.forward(50)
